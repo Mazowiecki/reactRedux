7cc75efa4236de520daa64ea3bb0558d357299f0
Stanislaw Mazowiecki <stmazowiecki@gmail.com>
2020-03-15 00:23:42 +0100

    Finished simple counter based on redux

---
+++ src/actions/index.js
@@ -0,0 +1,13 @@
+export const increment = (value = 1) => ({
+    type: 'INCREMENT',
+    value: value
+});
+
+export const decrement = (value = 1) => ({
+    type: 'DECREMENT',
+    value: value
+});
+
+export const reset = () => ({
+    type: 'RESET'
+});
--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import './App.css';
+import {useSelector, useDispatch} from "react-redux";
+import {decrement, increment, reset} from "../actions";
 
 function App() {
+    const counter = useSelector(state => state.counter);
+    const isLogged = useSelector(state => state.isLogged);
+    const dispach = useDispatch();
+
     return (
         <div className="App">
             <header className="App-header">
 
+                <div>
+                    <button onClick={() => dispach(increment())}>+</button>
+                    <button onClick={() => dispach(increment(5))}>+5</button>
+                    <button onClick={() => dispach(reset())}>Reset</button>
+                    <button onClick={() => dispach(decrement())}>-</button>
+                    <button onClick={() => dispach(decrement(5))}>-5</button>
+                </div>
+                <p>Counter: {counter}</p>
+
+                {isLogged ? <p>Valuable information only for logged users</p> : ''}
             </header>
         </div>
     );
